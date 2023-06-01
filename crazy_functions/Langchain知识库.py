f942716475681556a30e04f1a08049b8aabe1212
Ben <mr.ng@mystudies.net>
2023-06-01 14:08:02 +0800

    Update Langchain知识库.py

--- crazy_functions/Langchain知识库.py
+++ crazy_functions/Langchain知识库.py
@@ -15,7 +15,7 @@ def 知识库问答(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_pro
     web_port        当前软件运行的端口号
     """
     history = []    # 清空历史，以免输入溢出
-    chatbot.append(("这是什么功能？", "[Local Message] 从一批文件(txt, md, tex)中读取数据构建知识库, 然后进行问答。"))
+    chatbot.append(("這是什麼功能？ ", "[Local Message] 從一批文件(txt, md, tex)中讀取數據構建知識庫, 然後進行問答。"))
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
 
     # resolve deps
@@ -25,8 +25,8 @@ def 知识库问答(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_pro
         from .crazy_utils import knowledge_archive_interface
     except Exception as e:
         chatbot.append(
-            ["依赖不足", 
-             "导入依赖失败。正在尝试自动安装，请查看终端的输出或耐心等待..."]
+            ["依賴不足", 
+             "導入依賴失敗。正在嘗試自動安裝，請查看終端的輸出或耐心等待..."]
         )
         yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
         from .crazy_utils import try_install_deps
@@ -44,12 +44,12 @@ def 知识库问答(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_pro
         file_manifest += file_manifest_tmp
     
     if len(file_manifest) == 0:
-        chatbot.append(["没有找到任何可读取文件", "当前支持的格式包括: txt, md, docx, pptx, pdf, json等"])
+        chatbot.append(["沒有找到任何可讀取文件", "當前支持的格式包括: txt, md, docx, pptx, pdf, json等"])
         yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
         return
     
     # < -------------------预热文本向量化模组--------------- >
-    chatbot.append(['<br/>'.join(file_manifest), "正在预热文本向量化模组, 如果是第一次运行, 将消耗较长时间下载中文向量化模型..."])
+    chatbot.append(['<br/>'.join(file_manifest), "正在預熱文本向量化模組, 如果是第一次運行, 將消耗較長時間下載中文向量化模型..."])
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
     print('Checking Text2vec ...')
     from langchain.embeddings.huggingface import HuggingFaceEmbeddings
@@ -57,7 +57,7 @@ def 知识库问答(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_pro
         HuggingFaceEmbeddings(model_name="GanymedeNil/text2vec-large-chinese")
 
     # < -------------------构建知识库--------------- >
-    chatbot.append(['<br/>'.join(file_manifest), "正在构建知识库..."])
+    chatbot.append(['<br/>'.join(file_manifest), "正在構建知識庫..."])
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面
     print('Establishing knowledge archive ...')
     with ProxyNetworkActivate():    # 临时地激活代理网络
@@ -70,7 +70,7 @@ def 知识库问答(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_pro
     # chatbot._cookies['langchain_plugin_embedding'] = kai.get_current_archive_id()
     # chatbot._cookies['lock_plugin'] = 'crazy_functions.Langchain知识库->读取知识库作答'
     # chatbot.append(['完成', "“根据知识库作答”函数插件已经接管问答系统, 提问吧! 但注意, 您接下来不能再使用其他插件了，刷新页面即可以退出知识库问答模式。"])
-    chatbot.append(['构建完成', f"当前知识库内的有效文件：\n\n---\n\n{kai_files}\n\n---\n\n请切换至“知识库问答”插件进行知识库访问, 或者使用此插件继续上传更多文件。"])
+    chatbot.append(['構建完成', f"當前知識庫內的有效文件：\n\n---\n\n{kai_files}\n\n---\n\n請切換至“知識庫問答”插件進行知識庫訪問, 或者使用此插件繼續上傳更多文件。"])
     yield from update_ui(chatbot=chatbot, history=history) # 刷新界面 # 由于请求gpt需要一段时间，我们先及时地做一次界面更新
 
 @CatchException
