1206d84496e27f014ed289bc7e0b81619a4877c5
Ben <mr.ng@mystudies.net>
2023-07-02 18:43:43 +0800

    Update 辅助回答.py

--- crazy_functions/辅助回答.py
+++ crazy_functions/辅助回答.py
@@ -11,10 +11,10 @@ from crazy_functions.crazy_utils import request_gpt_model_in_new_thread_with_ui_
 def 猜你想问(txt, llm_kwargs, plugin_kwargs, chatbot, history, system_prompt, web_port):
     if txt:
         show_say = txt
-        prompt = txt+'\n回答完问题后，再列出用户可能提出的三个问题。'
+        prompt = txt+'\n回答完問題後，再列出用戶可能提出的三個問題。'
     else:
-        prompt = history[-1]+"\n分析上述回答，再列出用户可能提出的三个问题。"
-        show_say = '分析上述回答，再列出用户可能提出的三个问题。'
+        prompt = history[-1]+"\n分析上述回答，再列出用戶可能提出的三個問題。"
+        show_say = '分析上述回答，再列出用戶可能提出的三個問題。'
     gpt_say = yield from request_gpt_model_in_new_thread_with_ui_alive(
         inputs=prompt,
         inputs_show_user=show_say,
